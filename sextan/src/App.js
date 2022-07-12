d572ef8ed90b7da25b7ce356659ca4a205c9afc1
Selidrium <alangarcia1996@gmail.com>
2022-07-12 16:08:48 -0700

    Changes to be committed: 	new file:   sextan/src/AddressDisplay.js 	modified:   sextan/src/App.js 	modified:   sextan/src/Exhibit.css

--- sextan/src/App.js
+++ sextan/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import AddressDisplay from './AddressDisplay';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Banner bannerText="Sextant App" />
+                <Banner bannerText="Sextan App" />
                 <Exhibit name="Exhibit 1!"></Exhibit>
                 <Exhibit name="Exhibit 2!"></Exhibit>
-                <Exhibit name="Exhibit 3!"></Exhibit>
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
             </div>
         );
     }
